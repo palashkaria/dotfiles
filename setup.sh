7f8b92e4f94bda7d6b42ef3434153786c347ce17
Palash Karia <palash@locus.sh>
2020-07-14 01:01:40 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install apps and binaries with Brew
+source mac/brew.sh
+
+# Configure MacOS defaults.
+# You only want to run this once during setup. Additional runs may reset changes you make manually.
+source ./macos
