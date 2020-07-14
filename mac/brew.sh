299470155aa91fc48eedbffdb66e3876d406db2d
Palash Karia <palash@locus.sh>
2020-07-14 17:10:02 +0530

    Update brew.sh

--- mac/brew.sh
+++ mac/brew.sh
@@ -12,7 +12,7 @@ if [[ $? -ne 0 ]]; then
     # Accept Xcode license
     sudo xcodebuild -license
 fi
-
+ 
 # Update all OSX packages
 sudo softwareupdate -i -a
 
@@ -36,33 +36,40 @@ brew upgrade
 brew tap caskroom/cask
 
 ## Apps I use
-# brew cask install 1password # need beta
-# brew cask install brave
+
+brew cask install android-studio 
+brew cask install anybar
+brew cask install appcleaner
+brew cask install cryptomator
 brew cask install evernote
 brew cask install figma
-brew cask install google-drive
+
 brew cask install google-chrome #Chrome
 brew cask install homebrew/cask-versions/google-chrome-canary # Chrome Canary
-brew cask install microsoft-edge
 brew cask install kap
 brew cask install keycastr
+brew cask install microsoft-edge
+brew cask install postman
 brew cask install sketch
-brew cask install slack
-# brew cask install sourcetree
 brew cask install spotify
 brew cask install teamviewer
-bew cask install qlmarkdown
-bew cask install quicklook-csv
-bew cask install quicklook-json
-
-# brew cask install visual-studio-code
-# cryptomator
-# bettertouchtool
-# appCleaner
-# Amphtemine
-
+brew cask install qlmarkdown
+brew cask install quicklook-csv
+brew cask install quicklook-json
 # Remove outdated versions from the cellar.
 brew cleanup
 
+
 # App to keep mac awake (https://itunes.apple.com/pt/app/amphetamine/id937984704?mt=12)
 mas "Amphetamine", id: 937984704
+
+
+# brew cask install 1password # need bea
+# brew cask install brave
+# brew cask install google-backup-and-sync
+# brew cask install slack
+# brew cask install sourcetree
+# brew cask install visual-studio-code
+# brew cask install bettertouchtool
+# calibre
+# perimeter81
